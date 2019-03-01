c98b70574b21cc92ea946aae29f46a4308d60d4e
Slbalderrama <sergiob44_47@hotmail.com>
2019-03-01 14:12:32 +0100

    01/03/2019

--- MicroGrids/Regression.py
+++ MicroGrids/Regression.py
@@ -48,22 +48,6 @@ X['Diesel Cost'] = Data['Diesel Cost']
 X['Households'] = Data['Households']
 X['PV output'] = Data['PV output']
 
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(X['LLP'], X['Diesel Cost'], y, c='b')
-ax.set_xlabel('LLP (%)')
-ax.set_ylabel('Fuel Cost (USD/l)')
-ax.set_zlabel('LCOE (USD/kW)')
-
-pylab.xlim([0,6])
-pylab.ylim([0,1.2])
-ax.set_zlim(0, 0.5)
-ax.view_init(20,15)
-
-
-
 lm_4 = linear_model.LinearRegression(fit_intercept=True)
 model = lm_4.fit(X,y)
 
@@ -71,7 +55,7 @@ y_4_predictions = lm_4.predict(X)
 Score_Linear = round(lm_4.score(X,y),4)
 print('R_2 for linear regression is ' + str(Score_Linear*100) + ' %')
 
-kernel = RBF(length_scale=2)
+kernel =1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0))
 
 gp = gaussian_process.GaussianProcessRegressor(kernel=kernel)
 gp.fit(X, y)
@@ -83,19 +67,56 @@ Score_Gaussian = round(gp.score(X,y)*100)
 print('R_2 for gaussian regression is ' + str(Score_Gaussian)  + ' %')
 
 
+
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')
 #
-#LLP = [0,0.01, 0.05]
-#LOL = 
-#Diesel_Cost = 
-#range(80,210,12)
+#ax.scatter(X['LLP'], X['Diesel Cost'], y, c='b')
+#ax.set_xlabel('LLP (%)')
+#ax.set_ylabel('Fuel Cost (USD/l)')
+#ax.set_zlabel('LCOE (USD/kW)')
+#
+#pylab.xlim([0,6])
+#pylab.ylim([0,1.2])
+#ax.set_zlim(0, 0.5)
+#ax.view_init(20,15)
 
 
+llp = list(np.arange(0, 0.06, 0.01))
+Diesel_Cost = list(np.arange(0.28, 1.48, 0.2))
+Solar = range(10)
 
+X1 = pd.DataFrame()
+foo = 0
+for l in llp:
+    for d in Diesel_Cost:
+        for v in Village:
+            for s in Solar:
+                X1.loc[foo,'LLP'] = l*100
+                X1.loc[foo,'Diesel Cost'] = d
+                X1.loc[foo,'Households'] = v
+                X1.loc[foo,'PV output'] = s
+                foo += 1
 
+y_predictions_1= pd.DataFrame()                
+y_predictions_1['Regression'] = gp.predict(X1)
+y_predictions_1['LLP'] = X1['LLP']
+y_predictions_1['Diesel Cost'] = X1['Diesel Cost']
+y_predictions_1['Households'] = X1['Households']
+y_predictions_1['PV output'] = X1['PV output']
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
+ax.scatter(X1['LLP'], X1['Diesel Cost'], y_predictions_1['Regression'], c='b')
+ax.set_xlabel('LLP (%)')
+ax.set_ylabel('Fuel Cost (USD/l)')
+ax.set_zlabel('LCOE (USD/kW)')
+
+pylab.xlim([0,6])
+pylab.ylim([0,1.2])
+ax.set_zlim(0, 0.5)
+ax.view_init(20,45)
 
 
 
